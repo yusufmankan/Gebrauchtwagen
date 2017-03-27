a443e6d1269aeadf1bdd3825df210c28ef634e59
Yusuf <y.mankan99@htl-ottakring.ac.at>
2017-03-27 16:22:48 +0200

    Sql-Datenbank Einbindung

---
+++ Gebrauchtwagen/Gebrauchtwagen/Fahrzeug.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gebrauchtwagen
+{
+    class Fahrzeug
+    {
+        public string fid { get; set; }
+        public string marke { get; set; }
+        public string model { get; set; }
+        public string preis { get; set; }
+        public string kilometerstand { get; set; }
+        public string baujahr { get; set; }
+        public string treibstoff { get; set; }
+        public string getriebeart { get; set; }
+        public string leistung { get; set; }
+        public string beschreibung { get; set; }
+
+
+        public override string ToString()
+        {
+            return $"Fahrzeug: {fid} {marke} {model} {preis} {kilometerstand} {baujahr} {treibstoff} {getriebeart} {leistung} {beschreibung}";
+        }
+    }
+}
--- Gebrauchtwagen/Gebrauchtwagen/MainWindow.xaml.cs
+++ Gebrauchtwagen/Gebrauchtwagen/MainWindow.xaml.cs
@@ -1,10 +1,14 @@
-﻿using System;
+﻿using MySql.Data.MySqlClient;
+using static System.Diagnostics.Debug;
+using System.Windows;
+using System.Windows.Controls;
+using System;
+using System.Configuration;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
 using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
@@ -20,12 +24,59 @@ namespace Gebrauchtwagen
     /// </summary>
     public partial class MainWindow : Window
     {
+
+        private MySqlConnection conn;
+        private List<Fahrzeug> fahrzeuge;
         public MainWindow()
         {
-            InitializeComponent();
+            InitializeComponent();          
+            
+        }
 
-            
-            
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            var connstring = ConfigurationManager.AppSettings["connstring"];
+            conn = new MySqlConnection(connstring);
+            Anzeigen();
+        }
+
+        private void Anzeigen()
+        {
+            try
+            {
+                conn.Open();
+                var cmd = new MySqlCommand("SELECT Fid, Marke, Model, Preis, Kilometerstand, Baujahr, Treibstoff, Getriebeart, Leistung, Beschreibung FROM fahrzeug", conn);
+                fahrzeuge = new List<Fahrzeug>();
+                using (var rdr = cmd.ExecuteReader())
+                {
+                    while (rdr.Read())
+                    {
+                        var fahrzeuges = new Fahrzeug();
+                        if (rdr.IsDBNull(0))
+                            throw new Exception("Primary Key is Null");
+                        fahrzeuges.fid = rdr.GetString(0);
+                        fahrzeuges.marke = rdr.IsDBNull(1) ? "keine Daten" : rdr.GetString(1);
+                        fahrzeuges.model = rdr.IsDBNull(2) ? "keine Daten" : rdr.GetString(2);
+                        fahrzeuges.preis = rdr.IsDBNull(3) ? "keine Daten" : rdr.GetString(3);
+                        fahrzeuges.kilometerstand = rdr.IsDBNull(4) ? "keine Daten" : rdr.GetString(4);
+                        fahrzeuges.baujahr = rdr.IsDBNull(5) ? "keine Daten" : rdr.GetString(5);
+                        fahrzeuges.treibstoff = rdr.IsDBNull(6) ? "keine Daten" : rdr.GetString(6);
+                        fahrzeuges.getriebeart = rdr.IsDBNull(7) ? "keine Daten" : rdr.GetString(7);
+                        fahrzeuges.leistung = rdr.IsDBNull(8) ? "keine Daten" : rdr.GetString(8);
+                        fahrzeuges.beschreibung = rdr.IsDBNull(9) ? "keine Daten" : rdr.GetString(9);
+                        WriteLine(fahrzeuges.ToString());
+                        fahrzeuge.Add(fahrzeuges);
+
+                    }
+                }
+                dataGrid.ItemsSource = fahrzeuge;
+                conn.Close();
+            }
+
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
         }
 
         private void button_hochladen_Click(object sender, RoutedEventArgs e)
@@ -36,12 +87,13 @@ namespace Gebrauchtwagen
 
       
 
-        private void comboBox_Marke_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        
+
+        private void dataGrid_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
-            if(comboBox_Marke.SelectedValue.Equals("BMW"))
-            {
-                comboBox_Modell.Items.RemoveAt(0);
-            }
+            var fahrzeuges = dataGrid.SelectedItem as Fahrzeug;
+            WriteLine("Clicked: " + fahrzeuges?.ToString());
+            
         }
     }
 }
